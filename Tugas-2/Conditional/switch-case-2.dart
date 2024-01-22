7fe784062d57edf9f0d3410c6c78e40defb7afeb
Bagas Dwi Anggoro <bagasdwianggoro12@gmail.com>
2024-01-22 09:29:01 +0700

    switch-case-namaBulan

---
+++ Tugas-2/Conditional/switch-case-2.dart
@@ -0,0 +1,43 @@
+void main(){
+  var tanggal = 1;
+  var bulan = 1;
+  var tahun = 2024;
+  getTanggal(tanggal, bulan, tahun);
+}
+
+void getTanggal(var tanggal, var bulan, var tahun){
+  String namaBulan = getBulan(bulan);
+  String tanggalSekarang = tanggal.toString();
+  String tahunSekarang = tahun.toString();
+  print("Tanggal : "+tanggalSekarang+" "+namaBulan+" "+tahunSekarang);
+}
+
+String getBulan(int inputBulan){
+  switch (inputBulan) {
+    case 1:
+      return "Januari";
+    case 2:
+      return "Februari";
+    case 3:
+      return "Maret";
+    case 4:
+      return "April";
+    case 5:
+      return "Mei";
+    case 6:
+      return "Juni";
+    case 7:
+      return "Juli";
+    case 8:
+      return "Agustus";
+    case 9:
+      return "September";
+    case 10:
+      return "Oktober";
+    case 11:
+      return "November";
+    case 12:
+      return "Desember";
+    default: return "Invalid";
+  }
+}
